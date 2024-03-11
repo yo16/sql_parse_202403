7494fe1790dc6cdbaf87ee2aa6bcb986af868706
yo16 <xxyo1xx@gmail.com>
2024-03-11 15:50:24 +0900

    build環境を整えた

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import Sql2Ary from "./sql2ary";
+
+export {
+    Sql2Ary,
+};
---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const path = require('path')
+
+const isProd = process.argv.includes('production');
+const isTest = process.argv.includes('test');
+const subDir = isProd ? 'output/prod' : isTest ? 'output/test' : 'output/dev';
+const outputPath = path.join(__dirname, subDir);
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: outputPath,
+        devtoolModuleFilenameTemplate: info => path.resolve(__dirname, info.resourcePath),
+    },
+};

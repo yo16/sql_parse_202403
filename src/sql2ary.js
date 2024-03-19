08a7eccb88bf97d58630b6d5c1d07dd03e233260
yo16 <xxyo1xx@gmail.com>
2024-03-19 15:15:33 +0900

    複数のクエリを登録したときの対応

--- src/sql2ary.js
+++ src/sql2ary.js
@@ -15,18 +15,45 @@ const Sql2Ary = (query, database = "BigQuery") => {
         const {ast, tableList, columnList} = parser.parse(query, opt);
         stmt = {ast, tableList, columnList};
     } catch(e) {
-        console.error("Error");
+        console.error("Error at Parser.parse().");
         console.error("e.message");
+        throw new Error(`Error in Parsing-Process: ${e.message}`);
     }
     if (!stmt) {
         return null;
     }
 
     // array化
-    const stmtArray = Stmt2Ary(stmt);
+    let stmtArray = null;
+    try {
+        if (Array.isArray(stmt)) {
+            stmtArray = [];
+            stmt.forEach(s => {
+                const one_stmtArray = Stmt2Ary(s);
+                // 全部単純に結合する
+                // ★ todo: 同じテーブル名は、マージする？
+                // とりあえず１つのクエリのみを対象とする前提でおくが
+                // 万が一複数になっても落ちないようにこの暫定対応としておく
+                stmtArray = stmtArray.concat(one_stmtArray);
+            });
+        } else {
+            stmtArray = Stmt2Ary(stmt);
+        }
+    } catch(e) {
+        console.error("Error at Stmt2Ary().");
+        console.error(e.message);
+        throw new Error(`Error in Arraying-Process: ${e.message}`);
+    }
 
     // ConnectStmts
-    const stmtsObj = ConnectStmts(stmtArray);
+    let stmtsObj = null;
+    try {
+        stmtsObj = ConnectStmts(stmtArray);
+    } catch(e) {
+        console.error("Error at ConnectStmts().");
+        console.error(e.message);
+        throw new Error(`Error in Connecting-Process: ${e.message}`);
+    }
 
     return stmtsObj;
 };
